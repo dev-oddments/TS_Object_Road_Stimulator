4b32673f1ac6039ce4df2f5e67636546eaa712c3
YONGJAE LEE <yongjae.lee@nanocellect.com>
2020-10-05 03:40:25 +0900

    refactor: 논리적으로 맞지 않는 에러코드를 주석처리한다

--- src/Vehicle.ts
+++ src/Vehicle.ts
@@ -6,9 +6,9 @@ export default class Vehicle {
   private fuel: number;
 
   constructor(wheels: Wheel[], fuel: number) {
-    if (wheels.length <= -1) {
-      throw new Error('바퀴의 갯수는 "-1" 보다 커야 합니다.');
-    }
+    // if (wheels.length <= -1) {
+    //   throw new Error('바퀴의 갯수는 "-1" 보다 커야 합니다.');
+    // }
 
     if (fuel <= 0 || fuel > 100) {
       throw new Error('연료는 양은 0 ~ 100 사이 입니다.');
